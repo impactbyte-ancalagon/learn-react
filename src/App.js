a9d6424437d8d3eb5c6165e6458017cb82a53249
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-07 11:50:05 +0700

    Create Header component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
+import Header from "./Header"
+
+const numbers = [1, 2, 3]
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React with Ancalagon
-          </a>
-        </header>
+      <div>
+        {numbers.map(item => {
+          return <Header />
+        })}
       </div>
     )
   }

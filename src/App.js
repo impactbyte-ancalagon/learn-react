17a871e67967436dd5efa47c78c11cbe217023bd
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-07 13:54:30 +0700

    Refine App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 import Header from "./Header"
@@ -10,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>Hello React</h1>
         {numbers.map(item => {
           return <Header />
         })}

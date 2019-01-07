5cb8082a6a209519137682bb0a95a2e046bf0b83
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-07 15:19:26 +0700

    Make state simpler

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import "./App.css"
 import Header from "./Header"
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      counter: 0
-    }
+  state = {
+    counter: 0
   }
 
   click = async () => {
     // Wait for setState finished
     await this.setState({ counter: this.state.counter + 1 })
+
     console.log("CLICK!", this.state.counter)
   }
 
